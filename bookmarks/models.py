052632d78b1dee969f7f0a4c9e7af5fa45503ff9
kevin fricovsky <kevin@montylounge.com>
2009-09-27 04:01:10 +0800

    added slug field

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import urlparse
 
+from django.db.models import permalink
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
@@ -17,30 +18,26 @@ This not only enables more than one user to save the same URL as a
 bookmark but allows for per-user tagging.
 """
 
-# at the moment Bookmark has some fields that are determined by the
-# first person to add the bookmark (the adder) but later we may add
-# some notion of voting for the best description and note from
-# amongst those in the instances.
-
 class Bookmark(models.Model):
-    
+
     url = models.URLField(unique=True)
     description = models.CharField(_('description'), max_length=100)
+    slug = models.SlugField()
     note = models.TextField(_('note'), blank=True)
-    
+
     has_favicon = models.BooleanField(_('has favicon'))
     favicon_checked = models.DateTimeField(_('favicon checked'), default=datetime.now)
-    
+
     adder = models.ForeignKey(User, related_name="added_bookmarks", verbose_name=_('adder'))
     added = models.DateTimeField(_('added'), default=datetime.now)
-    
-    # tags = TagField()
-    
+
+    tags = TagField()
+
     def get_favicon_url(self, force=False):
         """
         return the URL of the favicon (if it exists) for the site this
         bookmark is on other return None.
-        
+
         If force=True, the URL will be calculated even if it doesn't
         exist.
         """
@@ -49,31 +46,40 @@ class Bookmark(models.Model):
             favicon_url = urlparse.urljoin(base_url, 'favicon.ico')
             return favicon_url
         return None
-    
+
     def all_tags(self, min_count=False):
         return Tag.objects.usage_for_model(BookmarkInstance, counts=False, min_count=None, filters={'bookmark': self.id})
-    
+
     def all_tags_with_counts(self, min_count=False):
         return Tag.objects.usage_for_model(BookmarkInstance, counts=True, min_count=None, filters={'bookmark': self.id})
-    
+
     def __unicode__(self):
         return self.url
-    
+
+    @permalink
+    def get_absolute_url(self):
+        return ('bookmark_detail', None, {
+            'year': self.added.year,
+            'month': self.added.strftime('%b').lower(),
+            'day': self.added.day,
+            'slug': self.slug
+        })
+
     class Meta:
         ordering = ('-added', )
 
 
 class BookmarkInstance(models.Model):
-    
+
     bookmark = models.ForeignKey(Bookmark, related_name="saved_instances", verbose_name=_('bookmark'))
     user = models.ForeignKey(User, related_name="saved_bookmarks", verbose_name=_('user'))
     saved = models.DateTimeField(_('saved'), default=datetime.now)
-    
+
     description = models.CharField(_('description'), max_length=100)
     note = models.TextField(_('note'), blank=True)
-    
+
     tags = TagField()
-    
+
     def save(self, force_insert=False, force_update=False):
         try:
             bookmark = Bookmark.objects.get(url=self.url)
@@ -83,12 +89,12 @@ class BookmarkInstance(models.Model):
             bookmark.save()
         self.bookmark = bookmark
         super(BookmarkInstance, self).save(force_insert, force_update)
-    
+
     def delete(self):
         bookmark = self.bookmark
         super(BookmarkInstance, self).delete()
         if bookmark.saved_instances.all().count() == 0:
             bookmark.delete()
-    
+
     def __unicode__(self):
         return _("%(bookmark)s for %(user)s") % {'bookmark':self.bookmark, 'user':self.user}
