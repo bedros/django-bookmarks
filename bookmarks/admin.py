229ce9205535850e8cf11252bb2a4d17f6e3c905
kevin fricovsky <kevin@montylounge.com>
2009-09-27 04:01:00 +0800

    added prepolate for new slug field

--- bookmarks/admin.py
+++ bookmarks/admin.py
@@ -3,6 +3,7 @@ from bookmarks.models import Bookmark, BookmarkInstance
 
 class BookmarkAdmin(admin.ModelAdmin):
     list_display = ('url', 'description', 'added', 'adder',)
+    prepopulated_fields = {'slug': ('description',)}
 
 admin.site.register(Bookmark, BookmarkAdmin)
 admin.site.register(BookmarkInstance)
